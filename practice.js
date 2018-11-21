3966a167f935f7a4998dae6a329c77145ea9d60e
VENKATASHIVA089 <r121089@rguktrkv.ac.in>
2018-11-21 20:11:23 +0530

    second_problem_correctly

--- practice.js
+++ practice.js
@@ -1,25 +1,41 @@
 var XLSX=require('xlsx')
+var Excel=require('exceljs')
 var wb=XLSX.readFile('./Book1.xlsx')
+//var wb2=new Excel.Workbook('./practicexlsx.xlsx');
 var ws=wb.Sheets.Sheet1
-let obj=XLSX.utils.sheet_to_json(wb.Sheets.Sheet1)
+//var ws2=wb2.getWorksheet('Sheet1')
+let array=XLSX.utils.sheet_to_json(wb.Sheets.Sheet1)
+//console.log(array)
+
 let output={}
-for(let x in obj){
-	if(output[obj[x].season]){
-		output[obj[x].season]++;
+for(let x in array){
+	if(output[array[x].season]){
+		output[array[x].season]++;
 	}
 	else{
-		output[obj[x].season]=1;	
+		output[array[x].season]=1;	
 	}
 }
 console.log(output)
-let output2={}
-for(let x in obj){
-	if(output2[obj[x].winner]){
-		output2[obj[x].winner]++;
+
+let output2={2008:{},
+			2009:{},
+			2010:{},
+			2011:{},
+			2012:{},
+			2013:{},
+			2014:{},
+			2015:{},
+			2016:{},
+			2017:{}
+}
+for(let x in array){
+	if(output2[array[x].season][array[x].winner]){
+		output2[array[x].season][array[x].winner]++;	
 	}
 	else{
-		output2[obj[x].winner]=1;	
+		if(array[x].winner)
+		output2[array[x].season][array[x].winner]=1;
 	}
 }
-delete output2.undefined
 console.log(output2)
